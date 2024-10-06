f3d8398ccf893fa1814414a6ee1e3e8a02bf50a3
XDsiroriginal <anna.sawhenko1984@gmail.com>
2024-10-06 19:42:50 +0700

    Update register_and_log.js

--- js/register_and_log.js
+++ js/register_and_log.js
@@ -7,7 +7,22 @@ function log() {
 	if (log == "admin") {
 		if (pass == "root") {
 			user = "admin";
-			document.location="index.html";
+			document.getElementById('log_div').style.display = "none";
+			document.getElementById("header_shop").style.display = "flex";
+			document.getElementById("body_shop").style.display = "flex";
+			document.getElementById("user_name").innerHTML = "Админ";
+		}
+		else {
+			alert("Неверынй логи или пороль");
+		}
+	}
+	else if (log == "vipuser") {
+		if (pass == "zxcclown") {
+			user = "vipuser";
+			document.getElementById('log_div').style.display = "none";
+			document.getElementById("header_shop").style.display = "flex";
+			document.getElementById("body_shop").style.display = "flex";
+			document.getElementById("user_name").innerHTML = "Вип юзер";
 		}
 		else {
 			alert("Неверынй логи или пороль");
@@ -17,3 +32,10 @@ function log() {
 		alert("Неверынй логи или пороль");
 	}
 }
+
+function adress_reg() {
+	console.log(document.getElementById('log_div').style.display);
+	document.getElementById('log_div').style.display = "flex";
+	document.getElementById("header_shop").style.display = "none";
+	document.getElementById("body_shop").style.display = "none";
+}	
